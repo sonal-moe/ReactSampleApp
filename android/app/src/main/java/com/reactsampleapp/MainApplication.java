baeed132e59a8fbea4a2c99d81c199805406ba81
Sonal Rathi <sonal.rathi@moengage.com>
2020-04-13 18:03:27 +0530

    ”ReactSampleapp”

---
+++ android/app/src/main/java/com/reactsampleapp/MainApplication.java
@@ -0,0 +1,64 @@
+package com.reactsampleapp;
+
+import android.app.Application;
+import android.content.Context;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.soloader.SoLoader;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import com.moengage.react.MoEReactPackage;
+import com.moengage.core.MoEngage;
+import com.moengage.core.Logger;
+import com.moe.pushlibrary.MoEHelper;
+import com.moengage.core.MoEngage;
+import com.moengage.react.MoEInitializer;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost =
+      new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+          return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+          @SuppressWarnings("UnnecessaryLocalVariable")
+          List<ReactPackage> packages = new PackageList(this).getPackages();
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
+          return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+          return "index";
+        }
+      };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    // this is the instance of the application class and "XXXXXXXXXXX" is the APP ID from the dashboard.
+ //moengage SDK initialisation
+  MoEngage moEngage =
+       new MoEngage.Builder(this, "DAO6UGZ73D9RTK8B5W96TPYN")
+       .setNotificationSmallIcon(R.drawable.ic_launcher)
+       .setNotificationLargeIcon(R.drawable.ic_launcher_round)
+       .setLogLevel(Logger.VERBOSE)
+       .build();
+   MoEInitializer.initialize(moEngage);
+
+  }
+}
